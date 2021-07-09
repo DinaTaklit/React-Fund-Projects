452855b692afa43f397782384424c79b5f50b3a4
DinaTaklit <ez_taklit@esi.dz>
2021-07-09 16:49:47 +0100

    3.Reviews: setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import BirthdayReminder from './prtojects/01-birthday-reminder'
 import Tours from './prtojects/2.Tours'
-
+import Reviews from './prtojects/3.Reviews'
 function App() {
   return (
-    <Tours/>
+    <Reviews/>
   );
 }
 
