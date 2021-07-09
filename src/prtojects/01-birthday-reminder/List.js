150416e9936be6e8e1510ce616f6c4b93f57cc6d
DinaTaklit <ez_taklit@esi.dz>
2021-07-09 07:50:41 +0100

    refac: 1.Birthday reminder: refact List

--- src/prtojects/01-birthday-reminder/List.js
+++ src/prtojects/01-birthday-reminder/List.js
@@ -5,13 +5,13 @@ const List = ({people}) => {
             people.map(person => {
                 const {id, name, age, image} = person
                 return (
-                    <div className="person" key={id}>
+                    <article className="person" key={id}>
                         <img src={image} alt={name} />
                         <div>
                             <h4>{name}</h4>
-                            <p>{age}</p>
+                            <p>{age} years</p>
                         </div>
-                    </div>
+                    </article>
                 )
             })
         }
