d1a99c7aae67f1b9cf6eb45b11dd327fdcd0ff6c
DinaTaklit <ez_taklit@esi.dz>
2021-07-09 07:47:13 +0100

    feat: 1.BirthdayReminder: Add list ofer person

--- src/prtojects/01-birthday-reminder/List.js
+++ src/prtojects/01-birthday-reminder/List.js
@@ -1,7 +1,20 @@
-const List = () => {
+const List = ({people}) => {
     return (
       <>
-        <h2>list component</h2>
+        {
+            people.map(person => {
+                const {id, name, age, image} = person
+                return (
+                    <div className="person" key={id}>
+                        <img src={image} alt={name} />
+                        <div>
+                            <h4>{name}</h4>
+                            <p>{age}</p>
+                        </div>
+                    </div>
+                )
+            })
+        }
       </>
     );
 };
