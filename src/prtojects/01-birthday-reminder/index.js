2365740c593d860c1a65ae5c4c7fb959293c74e8
DinaTaklit <ez_taklit@esi.dz>
2021-07-09 07:46:42 +0100

    feat: 1.BirthdayReminder: build the layout

--- src/prtojects/01-birthday-reminder/index.js
+++ src/prtojects/01-birthday-reminder/index.js
@@ -1,11 +1,20 @@
+import React, {useState} from 'react'
+import {data} from './data'
 import './index.css'
-
-import React from 'react'
+import List from './List'
 
 export default function Index() {
+    const [people, setPeople] = useState(data)
     return (
         <>
-            <h1>Birthday Reminder</h1>
+            <h2 style={{textAlign:"center", margin:"2rem auto 1rem", color:"whitesmoke"}}>Birthday Reminder</h2>
+            <main>
+                <section className="container">
+                    <h3>{people.length} birthday today</h3>
+                    <List people={people}/>
+                    <button onClick={() => setPeople([])}>Clear All</button>
+                </section>
+            </main>
         </>
     )
 }
