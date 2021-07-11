dbf7a4457949430e9c4f6472bf117ac6fdb3a87b
DinaTaklit <ez_taklit@esi.dz>
2021-07-11 16:42:51 +0100

    feat: 5.Menu: Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // import BirthdayReminder from './prtojects/01-birthday-reminder'
 // import ToursApp from './prtojects/2.Tours'
 // import Reviews from './prtojects/3.Reviews'
-import Accordion from './prtojects/4.Accordion'
+// import Accordion from './prtojects/4.Accordion'
+import Menu from './prtojects/5.Menu'
 
 function App() {
   return (
-    <Accordion/>
+    <Menu/>
   );
 }
 
---
+++ src/prtojects/5.Menu/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import './index.css'
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+
+function Index() {
+  return <h2>menu project setup</h2>;
+}
+
+export default Index;
