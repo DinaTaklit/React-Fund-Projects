7ea0100f2b10d3af203f80a023e0dd925472f4ba
DinaTaklit <ez_taklit@esi.dz>
2021-07-11 22:10:32 +0100

    feat: 5.Menu: add categories

--- src/prtojects/5.Menu/Categories.js
+++ src/prtojects/5.Menu/Categories.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({categories}) => {
+  return (
+    <div className="btn-container">
+      {
+        categories.map((category, index) => {
+          return (
+            <button 
+              key={index} 
+              className="filter-btn" 
+              onClick={() => {
+                console.log(category);
+            }}>{category}</button>
+          );
+        })
+      }
+    </div>
+  );
 };
 
 export default Categories;
--- src/prtojects/5.Menu/index.js
+++ src/prtojects/5.Menu/index.js
@@ -4,17 +4,19 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map(item => item.category))]
+
 
 function Index() {
   const [menuItems, setMenuItems] = useState(items)
-  const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState(allCategories)
   return (<main>
   <section className="menu section">
     <div className="title">
       <h2>Our Menu</h2>
       <div className="underline"></div>
     </div>
-    <Categories/>
+    <Categories categories={categories}/>
     <Menu items={menuItems}/>
   </section>
 </main>)
