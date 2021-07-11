2bf9bb85e1574d003cb76e2f66937f299d2a5c0e
DinaTaklit <ez_taklit@esi.dz>
2021-07-11 15:17:42 +0100

    feat: 4.Accordion: setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 // import BirthdayReminder from './prtojects/01-birthday-reminder'
 // import ToursApp from './prtojects/2.Tours'
-import Reviews from './prtojects/3.Reviews'
+// import Reviews from './prtojects/3.Reviews'
+import Accordion from './prtojects/4.Accordion'
+
 function App() {
   return (
-    <Reviews/>
+    <Accordion/>
   );
 }
 
