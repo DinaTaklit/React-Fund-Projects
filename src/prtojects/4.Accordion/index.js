55e5dde42f536839672b02933af74bd134ac0c99
DinaTaklit <ez_taklit@esi.dz>
2021-07-11 15:33:06 +0100

    feat: 4.Accordion: Add list of questions

--- src/prtojects/4.Accordion/index.js
+++ src/prtojects/4.Accordion/index.js
@@ -1,9 +1,27 @@
 import React, { useState } from 'react';
+import './index.css'
 import data from './data';
 import SingleQuestion from './Question';
 
 function Index() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data)
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions and Answers about login </h3>
+        <section className="info">
+          {
+            questions.map(question => {
+              return (
+                <SingleQuestion key={question.id} {...question} />
+              );
+            })
+          } 
+        </section>
+      </div>
+    </main>
+  )
+
 }
 
 export default Index;
