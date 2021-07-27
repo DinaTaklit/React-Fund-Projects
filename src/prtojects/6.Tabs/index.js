7a8fe1d62b2fec23a131b056300ee2edf040fea6
DinaTaklit <ez_taklit@esi.dz>
2021-07-27 05:28:39 +0100

    6.Tabs: implement tabs logic

--- src/prtojects/6.Tabs/index.js
+++ src/prtojects/6.Tabs/index.js
@@ -12,14 +12,64 @@ function Index() {
   const fetchJobs = async() => {
     const newJobs = await fetch(url).then(response => response.json())
     setJobs(newJobs)
+    setLoading(false)
   }
   useEffect(() => {
     fetchJobs()
-    setLoading(false)
   }, [])
 
+  if(loading){
+    return(
+      <section className='section loading'>
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+
+  const {company, dates, duties, title} = jobs[value]
   return (
-    <h1>Tabs</h1>
+    <section className='section'>
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        <div className="btn-container">
+          {
+            jobs.map((job, index) => {
+              return (
+                <button
+                  key={job.id}
+                  onClick={() => setValue(index)}
+                  className={`job-btn ${index === value && 'active-btn'}`}
+                >
+                  {job.company}
+                </button>
+              )
+            })
+          }
+        </div>
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+                <div className="job-desc" key={index}>
+                  <FaAngleDoubleRight className="job-icon">
+                  </FaAngleDoubleRight>
+                  <p>{duty}</p>
+                </div>
+              )
+            })
+          }
+        </article>
+      </div>
+      <button type="button" className="btn">
+          More info
+      </button>
+
+    </section>
   )
 }
 
