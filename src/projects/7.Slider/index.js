5dce33c7ef0b86b74f91312ee1658ffc4f16674c
DinaTaklit <ez_taklit@esi.dz>
2021-07-27 07:09:50 +0100

    feat: 7.Slider: Implement Slider

--- src/projects/7.Slider/index.js
+++ src/projects/7.Slider/index.js
@@ -5,7 +5,50 @@ import data from './data'
 import './index.css'
 
 function Index() {
-  return <h2>slider project setup</h2>;
+  const [people, setPeople] = useState(data)
+  const [index, setIndex] = useState(0)
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span> reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {
+          people.map((person, personIndex) => {
+            const {id, image, name, title, quote} = person
+
+            let position = "nextSlide"
+            if(personIndex === index){
+              position ="activeSlide"
+            }
+            if (
+              personIndex === index -1 || (index === 0 && personIndex === people.length - 1)
+            ) {
+              position = "lastSlide"
+            }
+
+            return(
+              <article className={position} key={id}>
+                <img src={image} alt={name} className="person-img" />
+                <h4>{name}</h4>
+                <p className="title">{title}</p>
+                <p className="text">{quote}</p>
+                <FaQuoteRight className="icon" />
+              </article>
+            )
+          })
+        }
+        <button className="prev">
+        <FiChevronLeft />
+        </button>
+        <button className="next">
+        <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default Index;
