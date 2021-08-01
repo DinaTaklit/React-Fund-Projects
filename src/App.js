b5f590b77bd1b57f4a1802e2080266d3113c5148
DinaTaklit <ez_taklit@esi.dz>
2021-08-01 11:53:39 +0100

    feat: 8.Nabar: setup

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@
 // import Accordion from './projects/4.Accordion'
 // import Menu from './projects/5.Menu'
 // import Tabs from './projects/6.Tabs'
-import Slider from './projects/7.Slider'
+// import Slider from './projects/7.Slider'
+import Navbar from './projects/8.Navbar'
 function App() {
   return (
-    <Slider/>
+    <Navbar/>
   );
 }
 
---
+++ src/projects/8.Navbar/index.js
@@ -0,0 +1,12 @@
+
+import Navbar from './Navbar'
+import './index.css'
+function Index() {
+  return (
+    <>
+      <h2>navbar project setup</h2>
+    </>
+  )
+}
+
+export default Index
