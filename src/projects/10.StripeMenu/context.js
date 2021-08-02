9dc1077a91ccafd3308db766193c208f8a44937b
DinaTaklit <ez_taklit@esi.dz>
2021-08-02 17:09:00 +0100

    feat: 10.StripeMenu: context: basic cimplementation

--- src/projects/10.StripeMenu/context.js
+++ src/projects/10.StripeMenu/context.js
@@ -1,2 +1,16 @@
-import { useState, useContext } from 'react'
+import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+const AppContext = React.createContext()
+
+const AppProvider = ({ children }) =>  {
+    return (
+        <AppContext.Provider value={{  }}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+// Custome hook to get useContext to avoid multiple imports
+export const useGlobalContext = () => useContext(AppContext)
+export {AppProvider} 
