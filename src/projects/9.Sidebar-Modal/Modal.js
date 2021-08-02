23e1e5414f0cfcf79265080894c308dcfbab0cb0
DinaTaklit <ez_taklit@esi.dz>
2021-08-02 09:48:29 +0100

    feat: 9.SidebarModal: Modal: basic implementation

--- src/projects/9.Sidebar-Modal/Modal.js
+++ src/projects/9.Sidebar-Modal/Modal.js
@@ -1,6 +1,15 @@
 import { FaTimes } from 'react-icons/fa'
 const Modal = () => {
-  return <h2>Modal</h2>
+  return (
+    <div className="modal-overlay">
+      <div className="modal-container">
+        <h2>modal content</h2>
+        <button className="close-modal-btn">
+          <FaTimes />
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Modal
