379e861f644b7b100291e3563ad77879fe589fc9
DinaTaklit <ez_taklit@esi.dz>
2021-08-02 12:22:12 +0100

    feat: 9.SidebarModal: wrap the components inside AppProvider

--- src/projects/9.Sidebar-Modal/index.js
+++ src/projects/9.Sidebar-Modal/index.js
@@ -2,13 +2,14 @@ import Modal from './Modal'
 import Sidebar from './Sidebar'
 import Home from './Home'
 import './index.css'
+import { AppProvider } from './context';
 function Index() {
   return (
-    <>
+    <AppProvider>
       <Home />
       <Modal />
       <Sidebar />
-    </>
+    </AppProvider>
   )
 }
 
