e8c3d0fcccb14151c962cd8da84f1b6b9d788dbd
DinaTaklit <ez_taklit@esi.dz>
2021-08-02 12:38:37 +0100

    feat: 9.SidebarModal: Sidebar: use context to show and close the sidebar

--- src/projects/9.Sidebar-Modal/Sidebar.js
+++ src/projects/9.Sidebar-Modal/Sidebar.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg'
 import { FaTimes } from 'react-icons/fa'
 import { social, links } from './data'
-
+import { useGlobalContext } from './context'
 const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext()
   return (
-    <aside className="sidebar">
+    <aside className={`sidebar ${isSidebarOpen ? 'show-sidebar' : ''}`}>
       <div className="sidebar-header">
         <img src={logo} alt="coding addict" className="logo" />
-        <button className="close-btn">
+        <button className="close-btn" onClick={closeSidebar}>
           <FaTimes />
         </button>
       </div>
