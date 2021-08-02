ede9b26e60e170b567f6b2965839f72e45baa724
DinaTaklit <ez_taklit@esi.dz>
2021-08-02 09:59:47 +0100

    feat: 9.SidebarModal: Sidebar: basic implementation

--- src/projects/9.Sidebar-Modal/Sidebar.js
+++ src/projects/9.Sidebar-Modal/Sidebar.js
@@ -3,7 +3,43 @@ import { FaTimes } from 'react-icons/fa'
 import { social, links } from './data'
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-header">
+        <img src={logo} alt="coding addict" className="logo" />
+        <button className="close-btn">
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map(link => {
+          const {id, url, text, icon} = link
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+      <ul className="social-icons">
+        {
+          social.map(link => {
+            const {id, url, icon} = link
+            return (
+              <li key={id}>
+                <a href={url}>
+                  {icon}
+                </a>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </aside>
+  )
 }
 
 export default Sidebar
