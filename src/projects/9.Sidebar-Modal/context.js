589011a4d5a3ccfe3c563d3665a54ff04d8bcc14
DinaTaklit <ez_taklit@esi.dz>
2021-08-02 12:20:19 +0100

    feat: 9.SidebarModal: implement context logic

--- src/projects/9.Sidebar-Modal/context.js
+++ src/projects/9.Sidebar-Modal/context.js
@@ -1,1 +1,32 @@
 import React, { useState, useContext } from 'react'
+
+const AppContext = React.createContext()
+
+const AppProvider = ({ children }) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+    const [isModalOpen, setIsModalOpen] = useState(false)
+
+    const openSidebar = () => setIsSidebarOpen(true)
+    const closeSidebar = () => setIsSidebarOpen(false)
+
+    const openModal = () => setIsModalOpen(true)
+    const closeModal = () => setIsModalOpen(false)
+
+    return (
+        <AppContext.Provider value={{
+            isSidebarOpen,
+            isModalOpen,
+            openSidebar,
+            closeSidebar,
+            openModal,
+            closeModal,
+        }}
+        >
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useGlobalContext = () => useContext(AppContext)
+export { AppContext, AppProvider}
+
