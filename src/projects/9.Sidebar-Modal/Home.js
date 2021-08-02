5d82133b207e4312c2094d1273055752c2911722
DinaTaklit <ez_taklit@esi.dz>
2021-08-02 12:26:11 +0100

    feat: 9.SidebarModal: Home: use context for buttons click

--- src/projects/9.Sidebar-Modal/Home.js
+++ src/projects/9.Sidebar-Modal/Home.js
@@ -1,14 +1,15 @@
 
-import { useContext } from 'react'
 import { FaBars } from 'react-icons/fa'
+import { useGlobalContext } from './context'
 
 const Home = () => {
+  const {openSidebar, openModal} = useGlobalContext()
   return (
     <main>
-      <button className="sidebar-toggle">
+      <button className="sidebar-toggle" onClick={openSidebar}>
         <FaBars />
       </button>
-      <button className="btn">
+      <button className="btn" onClick={openModal}>
         show modal
       </button>
     </main>
