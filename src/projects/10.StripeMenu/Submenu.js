a37e25d30cade93ff6528f35c4defdcbd1ff8720
DinaTaklit <ez_taklit@esi.dz>
2021-08-03 12:28:48 +0100

    feat: 10.StripeMenu: Submenu: impelement the logic that change the location of the submenu

--- src/projects/10.StripeMenu/Submenu.js
+++ src/projects/10.StripeMenu/Submenu.js
@@ -1,12 +1,42 @@
-import React, { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { useGlobalContext } from './context'
 
 const Submenu = () => {
-  const {isSubmenuOpen} = useGlobalContext()
+
+  // use context to get the global state
+  const {
+    isSubmenuOpen, 
+    page:{page, links},
+    location
+  } = useGlobalContext()
+
+  const submenuRef = useRef(null) // use a ref to store the submenu to change its location
+
+  // use useEffect to set the submenu location
+  useEffect(() => {
+    submenuRef.current.style.top = `${location.bottom}px`
+    submenuRef.current.style.left = `${location.center}px`
+  }, [page, location, links])
+
   return (
-    <aside className={`submenu ${isSubmenuOpen ? 'show':''}`}>
-      <section>
-        <h3>Submenu</h3>
+    <aside className={`submenu ${isSubmenuOpen ? 'show':''}`} ref={submenuRef}>
+      <section class="submenu-center">
+        <h4>{page}</h4>
+        <ul className="col-2">
+          {
+            links.map((link, index) => {
+            const {url, label, icon} = link
+             return (
+              <li key={index}>
+                <a href={url}>
+                  {icon}
+                  {label}
+                </a>
+              </li>
+             )
+            })
+          }
+        </ul>
       </section>
     </aside>
   )
