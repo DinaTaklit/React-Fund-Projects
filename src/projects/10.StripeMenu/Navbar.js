4f9ec75574dacd88385d9c52ae642a800598fbd9
DinaTaklit <ez_taklit@esi.dz>
2021-08-03 04:20:02 +0100

    feat: 10.StripeMenu: Navbar: implement the component

--- src/projects/10.StripeMenu/Navbar.js
+++ src/projects/10.StripeMenu/Navbar.js
@@ -2,7 +2,38 @@ import logo from './images/logo.svg'
 import { FaBars } from 'react-icons/fa'
 
 const Navbar = () => {
-  return <h2>navbar component</h2>
+  return (
+    <nav className="nav">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" className="nav-logo" />
+          <button className=" btn toggle-btn">
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <button className="link-btn">
+              products
+            </button>
+          </li>
+          <li>
+            <button className="link-btn">
+              developers
+            </button>
+          </li>
+          <li>
+            <button className="link-btn">
+              compagny
+            </button>
+          </li>
+        </ul>
+        <button className="btn signin-btn">
+          Sign in
+        </button>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
