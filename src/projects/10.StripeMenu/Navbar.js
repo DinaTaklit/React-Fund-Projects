3a4e076df39ecddda778e138544a76bc8c3854dd
DinaTaklit <ez_taklit@esi.dz>
2021-08-03 12:19:51 +0100

    feat: 10.StripeMenu: Navbar: implement display, close submenu logic

--- src/projects/10.StripeMenu/Navbar.js
+++ src/projects/10.StripeMenu/Navbar.js
@@ -1,18 +1,31 @@
 import logo from './images/logo.svg'
 import { FaBars } from 'react-icons/fa'
-
 import {useGlobalContext} from './context'
 
 const Navbar = () => {
-  const {openSidebar,openSubmenu} = useGlobalContext()
+
+  // use context to open sidebar, open close submenu
+  const {openSidebar,openSubmenu, closeSubmenu} = useGlobalContext() 
+
 
   // Function that handle the hover on the nav items
-  const handleSubmenu = (e) => {
-    
-    openSubmenu()
+  const displaySubmenu = (e) => {
+    const page = e.target.textContent // get the current selected nav item link
+    const tempBtn = e.target.getBoundingClientRect() // to get the coordinates of the selected link
+    const center = (tempBtn.left + tempBtn.right) / 2 // to fix the submenu in the center of the link 
+    const bottom = tempBtn.bottom - 3 // to fix the submenu in the bottom of the link
+    openSubmenu(page, {center, bottom}) // open the submenu
   }
+
+  // Function that handle the close of submenu once the mouse is out of links
+  const handleCloseSubmenu= (e) => {
+    if (!e.target.classList.contains('link-btn')){
+      closeSubmenu()
+    }
+  }
+
   return (
-    <nav className="nav">
+    <nav className="nav" onMouseOver={handleCloseSubmenu}>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" className="nav-logo" />
@@ -22,18 +35,18 @@ const Navbar = () => {
         </div>
         <ul className="nav-links">
           <li>
-            <button className="link-btn" onMouseOver={handleSubmenu}>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
               products
             </button>
           </li>
           <li>
-            <button className="link-btn" onMouseOver={handleSubmenu}>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
               developers
             </button>
           </li>
           <li>
-            <button className="link-btn" onMouseOver={handleSubmenu}>
-              compagny
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              company
             </button>
           </li>
         </ul>
