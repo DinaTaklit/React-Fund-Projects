15b261a3a65571ab6d81cf2d143441625eecdc31
DinaTaklit <ez_taklit@esi.dz>
2021-08-03 04:50:54 +0100

    feat: 10.StripeMenu: context: implement basic Sidebar, Submenu opening closing logic

--- src/projects/10.StripeMenu/context.js
+++ src/projects/10.StripeMenu/context.js
@@ -4,8 +4,28 @@ import sublinks from './data'
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) =>  {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false) // to open and closde the sidebar
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false) // to open and close the submenu
+    
+    // Functions to open and close Sidebar
+    const openSidebar = () => setIsSidebarOpen(true)
+    const closeSidebar = () => setIsSidebarOpen(false)
+
+    // Functions to show and hide Submenu
+    const openSubmenu = () => {
+        setIsSidebarOpen(true)
+    }
+    const closeSubmenu = () => setIsSubmenuOpen(false)
+
     return (
-        <AppContext.Provider value={{  }}>
+        <AppContext.Provider value={{  
+            isSidebarOpen,
+            openSidebar,
+            closeSidebar,
+            isSubmenuOpen,
+            closeSubmenu,
+            openSubmenu,
+        }}>
             {children}
         </AppContext.Provider>
     )
