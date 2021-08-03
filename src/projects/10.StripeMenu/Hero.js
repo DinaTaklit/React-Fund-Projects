15e3717306d857ec8fb771a803f7bafe4d911a13
DinaTaklit <ez_taklit@esi.dz>
2021-08-03 04:12:25 +0100

    feat: 10.StripeMenu: Sidebar: implement the component

--- src/projects/10.StripeMenu/Hero.js
+++ src/projects/10.StripeMenu/Hero.js
@@ -6,10 +6,10 @@ const Hero = () => {
     <section className="hero">
       <div className="hero-center">
         <article className="hero-info">
-          <h1>
+          <h2>
             Payments infrastructure <br />
             for the internet
-          </h1>
+          </h2>
           <p>
             Millions of companies of all sizes—from startups to Fortune 500s—use
             Stripe’s software and APIs to accept payments, send payouts, and
--- src/projects/10.StripeMenu/Sidebar.js
+++ src/projects/10.StripeMenu/Sidebar.js
@@ -2,7 +2,36 @@ import { FaTimes } from 'react-icons/fa'
 import sublinks from './data'
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
+  return (
+    <div className="sidebar-wrapper">
+      <aside className="sidebar">
+        <button className="close-btn">
+          <FaTimes />
+        </button>
+        <div className="sidebar-links">
+          {sublinks.map((item, index) => {
+            const {links, page} = item
+            return (
+              <article key={index}>
+                <h3>{page}</h3>
+                <div className="sidebar-sublinks">
+                  {links.map((link, index) => {
+                    const {url, icon, label} = link
+                    return (
+                      <a key={index} href={url}>
+                        {icon}
+                        {label}
+                      </a>
+                    )
+                  })}
+                </div>
+              </article>
+            )
+          })}
+        </div>
+      </aside>
+    </div>
+  )
 }
 
 export default Sidebar
