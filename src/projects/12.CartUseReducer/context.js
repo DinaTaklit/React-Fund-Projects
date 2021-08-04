b7732b53a95d8564044c68efa79c9e0213f36a19
DinaTaklit <ez_taklit@esi.dz>
2021-08-04 11:15:33 +0100

    feat: 12.CartUseReducer: context: UseReducer Initial State

--- src/projects/12.CartUseReducer/context.js
+++ src/projects/12.CartUseReducer/context.js
@@ -5,13 +5,22 @@ import reducer from './reducer'
 const url = 'https://course-api.com/react-useReducer-cart-project'
 const AppContext = React.createContext()
 
+// Reducer initial state
+const initialState = {
+  loading: false,
+  cart: cartItems,
+  total: 0,
+  amount: 0
+}
+
 const AppProvider = ({ children }) => {
-  const [cart, setCart] = useState(cartItems)
+  
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   return (
     <AppContext.Provider
       value={{
-        cart,
+        ...state,
       }}
     >
       {children}
