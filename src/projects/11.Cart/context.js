8e9de798e29697d0652c7c5a17b839d40b3a33d7
DinaTaklit <ez_taklit@esi.dz>
2021-08-04 08:26:12 +0100

    feat: 11.Cart: Cart: implement clear cart functionality

--- src/projects/11.Cart/context.js
+++ src/projects/11.Cart/context.js
@@ -10,11 +10,16 @@ const AppContext = React.createContext()
 const AppProvider = ({ children }) => {
   const [cart, setCart] = useState(cartItems)
 
+  const clearCart = () =>{
+    setCart([])
+  }
+
   return (
     <AppContext.Provider
       value={{
         cart,
-      }}
+        clearCart,
+      }} 
     >
       {children}
     </AppContext.Provider>
